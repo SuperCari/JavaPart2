454f3a91991ed09d1a87cfd94c96658dec52dfa9
SuperCari <carina.coelestin@edu.fh-joanneum.at>
2016-11-04 16:33:16 +0100

    Sis is a commit

--- src/fh/java/geometry/Main.java
+++ src/fh/java/geometry/Main.java
@@ -10,18 +10,93 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-		//TODO: main() is missing
 
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.doIT();
+    }
+
+    private void doIT() {
+        List <GeometricShape> shapeList = new ArrayList();
+
+        fillList(shapeList);
+
+        printList();
+
+        List<Circle> circleList = new ArrayList<>();
+        for (GeometricShape shape : shapeList) {
+            if (shape instanceof Circle) {
+                circleList.add((Circle)shape);
+            }
+        }
+        System.out.println("-----circles------");
+        for(Circle circle: circleList){
+            System.out.println(circle);
+        }
+    }
 
     public void fillList(List<GeometricShape> shapeList) {
 		//TODO: Implement
+        shapeList.add(new Circle(1,2,2.0));
+        shapeList.add(new Circle(2,1,2.0));
+        shapeList.add(new Circle(3,2,5.0));
+
+        shapeList.add(new RightTriangle(11,22,10.0,11.0));
+        shapeList.add(new RightTriangle(11,22,10.0,11.0));
+        shapeList.add(new RightTriangle(11,22,10.0,11.0));
+
+        shapeList.add(new Rectangle(1,2,20.1,21.11));
 
     }
 
 
     public void printList(List<GeometricShape> shapeList) {
 		//TODO: Implement
+    System.out.println("----forloop------");
+        for (int pos = 0; pos <  shapeList.size(); pos++) {
+            System.out.println(shapeList.get(pos));
+        }
+
+        System.out.println("-----for each------------");
+        for (GeometricShape shape : shapeList) {
+            System.out.println(shape);
+
+        }
+
+        shapeList.stream()
+                .forEach(shape-> System.out.println(shape));
+
+        shapeList.stream()
+                .forEach(System.out::println("lllllllllllaaaaaaaa"));
 
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
